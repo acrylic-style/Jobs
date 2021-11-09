b58a799016e1b88f98bee379a608f1e5b858afaa
acrylic-style <me@acrylicstyle.xyz>
2021-11-09 20:55:43 +0900

    Enforce max-level of job

---
+++ README.md
@@ -0,0 +1,10 @@
+# [Jobs](https://github.com/Zrips/Jobs)
+
+## Building
+`./patcher p && cd Jobs && mvn package`
+
+### Requirements (for building)
+- bash
+- git
+- maven
+- jdk 8+
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 export target="$1"
 export branch="$2"
 declare -A GH_URL
-GH_URL[someProject]='https://github.com/foo/bar'
+GH_URL[Jobs]='https://github.com/Zrips/Jobs'
 if [ ! -e "work/$target" ]; then
   mkdir -p "work"
   git clone "${GH_URL[$target]}" "work/$target"
@@ -12,8 +12,9 @@ fi
   # git submodule update --init &&
   cd "work/$target" &&
   git fetch --all &&
-  git checkout "$branch" &&
-  git reset --hard "origin/$branch" &&
+  git checkout "v4.17.2" &&
+  git branch -d "$branch" ;
+  git switch -c "$branch" &&
   cd ../.. &&
   echo "Updated data"
 ) || exit 1
